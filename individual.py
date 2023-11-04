a9f1173ea65b4491a33a09f24e4b32f519cc8834
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-11-04 02:42:44 -0400

    Update individual.py

--- individual.py
+++ individual.py
@@ -1,37 +1,35 @@
-from genome import Gene, Genome
 import numpy as np
 from neural_network import create_neural_network_model
 
 class Individual:
-    def __init__(self, genome):
+    def __init__(self, genome, seq_length, d_model):
         self.genome = genome
         self.fitness = 0
         self.energy = 100  # Initial energy
         self.health = 100  # Initial health
-        self.neural_network_model = create_neural_network_model()
+        self.neural_network_model = create_neural_network_model(seq_length, d_model)
     
     def process_sensor_data(self, sensor_data):
-        # Simulate processing of sensor data based on individual's genome
+        # Process sensor data based on individual's genome
         processed_data = np.tanh(sensor_data + self.process_genes_for_sensor_data())
-        # Ensure the processed data is in the correct format for the neural network model input
+        # Format for neural network input
         processed_data = np.expand_dims(processed_data, axis=0)
         return processed_data
     
-    
     def process_genes_for_sensor_data(self):
-        # Example implementation: sum the weights of all genes connected to sensors for sensor data processing
+        # Sum the weights of genes connected to sensors
         sensor_processing_value = sum(gene.weight for gene in self.genome.genes if gene.source_type == 1)
         return sensor_processing_value
 
     def make_decision(self, processed_sensor_data):
-        # Utilize the neural network model for decision-making
+        # Decision-making with neural network
         prediction = self.neural_network_model.predict(processed_sensor_data)
-        decision = prediction[0][0]  # Assuming the first output is the decision
-        reward = prediction[1][0]  # Assuming the second output is the reward
+        decision = prediction[0][0]  # Assuming first output is decision
+        reward = prediction[1][0]  # Assuming second output is reward
         return decision, reward
     
     def interact_with_robot(self, decision):
-        # Simulate interaction with the bipedal robot
+        # Interaction with bipedal robot
         if decision > 0.5:
             print("Moving Forward")
         elif decision < -0.5:
@@ -40,5 +38,6 @@ class Individual:
             print("Standing Still")
     
     def evaluate_fitness(self, reward):
-        # Simulate fitness evaluation based on individual's state, decisions, and received reward
-        self.fitness = self.energy + self.health + reward  # Simplified fitness function
+        # Fitness evaluation
+        self.fitness = self.energy + self.health + reward
+
