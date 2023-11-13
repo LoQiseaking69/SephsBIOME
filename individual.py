528b14ea5025098e08577e66e69864566762f8bb
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-11-13 15:41:36 -0500

    Update individual.py
    Kinematic adjustments.

--- individual.py
+++ individual.py
@@ -8,16 +8,16 @@ class Individual:
         self.energy = 100  # Initial energy
         self.health = 100  # Initial health
         self.neural_network_model = create_neural_network_model(seq_length, d_model)
-        self.decision_log = []  # Add this line to log decisions
-        self.reward_log = []    # Add this line to log rewards
-    
+        self.decision_log = []  # Log decisions
+        self.reward_log = []    # Log rewards
+
     def process_sensor_data(self, sensor_data):
         # Process sensor data based on individual's genome
         processed_data = np.tanh(sensor_data + self.process_genes_for_sensor_data())
         # Format for neural network input
         processed_data = np.expand_dims(processed_data, axis=0)
         return processed_data
-    
+
     def process_genes_for_sensor_data(self):
         # Sum the weights of genes connected to sensors
         sensor_processing_value = sum(gene.weight for gene in self.genome.genes if gene.source_type == 1)
@@ -34,17 +34,29 @@ class Individual:
         self.reward_log.append(reward)      # Log reward
 
         return decision, reward
-    
+
     def interact_with_robot(self, decision):
-        # Interaction with bipedal robot
-        if decision > 0.5:
-            print("Moving Forward")
-        elif decision < -0.5:
-            print("Moving Backward")
-        else:
-            print("Standing Still")
-    
+        # Advanced interaction with bipedal robot for kinematic learning
+        if decision == 0:
+            self.adjust_step_length()
+        elif decision == 1:
+            self.adjust_balance()
+        # Add more conditions for different kinematic adjustments
+
+    def adjust_step_length(self):
+        # Logic for adjusting step length
+        print("Adjusting Step Length")
+        # Implementation details here
+
+    def adjust_balance(self):
+        # Logic for adjusting balance
+        print("Adjusting Balance")
+        # Implementation details here
+
     def evaluate_fitness(self, reward):
         # Fitness evaluation
         self.fitness = self.energy + self.health + reward
+
+    # Additional methods for other kinematic adjustments can be added here
+
 
