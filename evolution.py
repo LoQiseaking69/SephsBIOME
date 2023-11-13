28265db14b5f46ad87144e432101605a24011c39
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-11-13 16:26:27 -0500

    Update evolution.py
    d_model

--- evolution.py
+++ evolution.py
@@ -6,11 +6,11 @@ class Evolution:
     def __init__(self, population_size, mutation_rate, seq_length, d_model):
         self.population_size = population_size
         self.mutation_rate = mutation_rate
-        # Updated to pass seq_length and d_model to Individual
+        self.seq_length = seq_length
+        self.d_model = d_model
         self.population = [Individual(Genome(), seq_length, d_model) for _ in range(population_size)]
 
     def select_parents(self):
-        # Select two parents based on fitness proportional selection
         total_fitness = sum(individual.fitness for individual in self.population)
         selection_probs = [individual.fitness / total_fitness for individual in self.population]
         parents = random.choices(self.population, weights=selection_probs, k=2)
@@ -20,16 +20,28 @@ class Evolution:
         new_population = []
         for _ in range(self.population_size):
             parent1, parent2 = self.select_parents()
-            # Perform crossover and mutation to produce offspring
             offspring_genome = parent1.genome.crossover(parent2.genome)
             offspring_genome.mutate(self.mutation_rate)
-            # Create a new Individual with the new genome
-            offspring = Individual(offspring_genome, parent1.seq_length, parent1.d_model)
+            offspring = Individual(offspring_genome, self.seq_length, self.d_model)
             new_population.append(offspring)
         self.population = new_population
+
+    def evaluate_fitness(self, individual):
+        individual.fitness = self.calculate_control_efficiency(individual)
+
+    def calculate_control_efficiency(self, individual):
+        # Fitness based on control efficiency of motors, sensors, and actuators
+        control_efficiency = 0
+        # Implement logic to calculate control efficiency
+        # This could involve metrics like response time, accuracy, power consumption, etc.
+        # Example: control_efficiency = individual.evaluate_control_performance()
+        return control_efficiency
 
     def run_evolution(self, generations):
         for _ in range(generations):
             self.create_next_generation()
-            # Optional: Add code to assess and print population fitness after each generation
+            for individual in self.population:
+                self.evaluate_fitness(individual)
+                print(f"Individual Fitness: {individual.fitness}")
 
+# The Individual class should implement a method like evaluate_control_performance
