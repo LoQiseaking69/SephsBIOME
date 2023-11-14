c3dcfe04c07752a789b51d10a534753195688579
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-11-13 19:41:25 -0500

    Update individual.py

--- individual.py
+++ individual.py
@@ -3,7 +3,7 @@ from neural_network import create_neural_network_model
 
 class Individual:
     def __init__(self, genome, seq_length, d_model):
-        self.genome = genome
+        self.genome = genome  # Genome encoding kinematic traits
         self.fitness = 0
         self.energy = 100  # Initial energy
         self.health = 100  # Initial health
@@ -12,50 +12,92 @@ class Individual:
         self.reward_log = []    # Log rewards
 
     def process_sensor_data(self, sensor_data):
-        # Process sensor data based on individual's genome
-        processed_data = np.tanh(sensor_data + self.process_genes_for_sensor_data())
-        # Format for neural network input
+        # Incorporate genome data in sensor processing
+        genome_influence = self.process_genes_for_sensor_data()
+        processed_data = np.tanh(sensor_data + genome_influence)
         processed_data = np.expand_dims(processed_data, axis=0)
         return processed_data
 
     def process_genes_for_sensor_data(self):
-        # Sum the weights of genes connected to sensors
-        sensor_processing_value = sum(gene.weight for gene in self.genome.genes if gene.source_type == 1)
-        return sensor_processing_value
+        # Utilize genome data for sensory processing
+        sensor_processing_value = sum(gene.weight for gene in self.genome.genes if gene.source_type == 'sensor')
+        kinematic_influence = sum(gene.kinematic_trait for gene in self.genome.genes)
+        return sensor_processing_value + kinematic_influence
 
     def make_decision(self, processed_sensor_data):
-        # Use the neural network model to make a decision and get a reward
+        # Decision-making via neural network model
         prediction = self.neural_network_model.predict(processed_sensor_data)
-        decision = prediction[0][0]  # Assuming first output is decision
-        reward = prediction[1][0]  # Assuming second output is reward
+        decision = prediction[0][0]  # First output as decision
+        reward = prediction[1][0]  # Second output as reward
 
-        # Log the decision and reward
-        self.decision_log.append(decision)  # Log decision
-        self.reward_log.append(reward)      # Log reward
+        self.decision_log.append(decision)
+        self.reward_log.append(reward)
 
         return decision, reward
 
     def interact_with_robot(self, decision):
-        # Advanced interaction with bipedal robot for kinematic learning
-        if decision == 0:
+        # Implement complex kinematic adjustments based on decision
+        if decision == 'adjust_step_length':
             self.adjust_step_length()
-        elif decision == 1:
+        elif decision == 'adjust_balance':
             self.adjust_balance()
-        # Add more conditions for different kinematic adjustments
+        # Additional complex kinematic adjustments
 
     def adjust_step_length(self):
-        # Logic for adjusting step length
-        print("Adjusting Step Length")
-        # Implementation details here
+        # Complex logic to adjust step length based on genome traits
+        step_length_trait = self.extract_trait('step_length')
+        new_step_length = self.calculate_step_length(step_length_trait)
+        print(f"Adjusting Step Length to {new_step_length}")
+        # Implement complex action to adjust step length in the robot's control system
 
     def adjust_balance(self):
-        # Logic for adjusting balance
-        print("Adjusting Balance")
-        # Implementation details here
+        # Complex logic to adjust balance based on genome traits
+        balance_trait = self.extract_trait('balance')
+        balance_adjustment = self.calculate_balance(balance_trait)
+        print(f"Adjusting Balance by {balance_adjustment}")
+        # Implement complex action to adjust balance in the robot's control system
+
+    def extract_trait(self, trait_name):
+        # Extract specific kinematic trait from genome
+        trait_values = [gene.kinematic_trait for gene in self.genome.genes if gene.source_type == trait_name]
+        return np.mean(trait_values) if trait_values else 0
+
+    def calculate_step_length(self, trait):
+        # Advanced calculation for step length
+        base_step_length = 0.5  # Base step length in meters
+        trait_influence = trait * 0.15  # Greater influence of the trait
+        return base_step_length + trait_influence
+
+    def calculate_balance(self, trait):
+        # Advanced calculation for balance adjustment
+        balance_factor = 1.0  # Base balance factor
+        trait_influence = trait * 0.1  # Greater influence of the trait
+        return balance_factor + trait_influence
 
     def evaluate_fitness(self, reward):
-        # Fitness evaluation
-        self.fitness = self.energy + self.health + reward
+        # Advanced fitness evaluation considering kinematic efficiency
+        stability_score = self.calculate_stability()
+        agility_score = self.calculate_agility()
+        kinematic_efficiency = self.calculate_kinematic_efficiency()
+        self.fitness = self.energy + self.health + reward + stability_score + agility_score + kinematic_efficiency
+
+    def calculate_stability(self):
+        # Advanced calculation for stability score
+        stability_traits = [gene.weight for gene in self.genome.genes if gene.sink_type == 'stability']
+        return np.mean(stability_traits) * 15  # Increased weightage
+
+    def calculate_agility(self):
+        # Advanced calculation for agility score
+        agility_traits = [gene.weight for gene in self.genome.genes if gene.sink_type == 'agility']
+        return np.mean(agility_traits) * 15  # Increased weightage
+
+    def calculate_kinematic_efficiency(self):
+        # Calculation for overall kinematic efficiency
+        efficiency_traits = [gene.kinematic_trait for gene in self.genome.genes if gene.source_type in ['step_length', 'balance']]
+        return np.mean(efficiency_traits) * 20  # Significant weightage
+
+    # Additional methods for more complex kinematic adjustments
+
 
     # Additional methods for other kinematic adjustments can be added here
 
