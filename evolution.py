d13b3db619607bbbfacf45042c9807d47120ffe8
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-11-13 19:53:35 -0500

    Update evolution.py
    First ros entry.

--- evolution.py
+++ evolution.py
@@ -1,47 +1,80 @@
-import random
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image  # Adjust the message type based on your sensor data
 from individual import Individual
 from genome import Genome
 
+class ROSInterface:
+    def __init__(self):
+        # Initialize the ROS node
+        rospy.init_node('evolution_node', anonymous=True)
+
+        # Initialize ROS subscribers and publishers
+        self.sensor_data_subscriber = rospy.Subscriber("/sensor_data", Image, self.sensor_data_callback)
+        self.control_publisher = rospy.Publisher("/control_command", String, queue_size=10)
+
+        # Store the latest sensor data
+        self.latest_sensor_data = None
+
+    def sensor_data_callback(self, data):
+        # Update the latest sensor data on receiving new data
+        self.latest_sensor_data = data
+
+    def get_sensor_data(self):
+        # Provide the latest sensor data
+        return self.latest_sensor_data
+
+    def send_control_command(self, command):
+        # Send control commands to the robot
+        self.control_publisher.publish(command)
+
 class Evolution:
-    def __init__(self, population_size, mutation_rate, seq_length, d_model):
+    def __init__(self, population_size, seq_length, d_model):
         self.population_size = population_size
-        self.mutation_rate = mutation_rate
         self.seq_length = seq_length
         self.d_model = d_model
+        self.ros_interface = ROSInterface()  # Interface with ROS
         self.population = [Individual(Genome(), seq_length, d_model) for _ in range(population_size)]
 
-    def select_parents(self):
-        total_fitness = sum(individual.fitness for individual in self.population)
-        selection_probs = [individual.fitness / total_fitness for individual in self.population]
-        parents = random.choices(self.population, weights=selection_probs, k=2)
-        return parents
+    def run_generation(self):
+        # Simulate a generation of individuals
+        for individual in self.population:
+            self.simulate_individual(individual)
+
+        # Sort and select the top-performing individuals
+        self.population.sort(key=lambda x: x.fitness, reverse=True)
+        self.population = self.population[:self.population_size // 2]
+
+        # Create the next generation
+        next_generation = self.create_next_generation()
+        self.population = next_generation
+
+    def simulate_individual(self, individual):
+        # Get sensor data from ROS and simulate individual's performance
+        sensor_data = self.ros_interface.get_sensor_data()
+        if sensor_data:
+            processed_data = individual.process_sensor_data(sensor_data)
+            decision, reward = individual.make_decision(processed_data)
+            control_command = individual.determine_control_command(decision)
+            self.ros_interface.send_control_command(control_command)
+            individual.evaluate_fitness(reward)
 
     def create_next_generation(self):
-        new_population = []
-        for _ in range(self.population_size):
-            parent1, parent2 = self.select_parents()
-            offspring_genome = parent1.genome.crossover(parent2.genome)
-            offspring_genome.mutate(self.mutation_rate)
-            offspring = Individual(offspring_genome, self.seq_length, self.d_model)
-            new_population.append(offspring)
-        self.population = new_population
-
-    def evaluate_fitness(self, individual):
-        individual.fitness = self.calculate_control_efficiency(individual)
-
-    def calculate_control_efficiency(self, individual):
-        # Fitness based on control efficiency of motors, sensors, and actuators
-        control_efficiency = 0
-        # Implement logic to calculate control efficiency
-        # This could involve metrics like response time, accuracy, power consumption, etc.
-        # Example: control_efficiency = individual.evaluate_control_performance()
-        return control_efficiency
+        # Generate the next generation of individuals
+        next_generation = []
+        while len(next_generation) < self.population_size:
+            parent1, parent2 = random.sample(self.population, 2)
+            child_genome = parent1.genome.crossover(parent2.genome)
+            child_genome.mutate(mutation_rate=0.1)
+            child = Individual(child_genome, self.seq_length, self.d_model)
+            next_generation.append(child)
+        return next_generation
 
     def run_evolution(self, generations):
+        # Run the evolutionary process for a set number of generations
         for _ in range(generations):
-            self.create_next_generation()
-            for individual in self.population:
-                self.evaluate_fitness(individual)
-                print(f"Individual Fitness: {individual.fitness}")
+            self.run_generation()
 
-# The Individual class should implement a method like evaluate_control_performance
+# Example usage
+evolution = Evolution(population_size=100, seq_length=10, d_model=32)
+evolution.run_evolution(generations=50)
