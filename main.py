a108958ed5ec17acca19d2f64dd19c78ab35f188
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-11-16 09:10:49 -0500

    optimized main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-import time
 import logging
 from simulator import Simulator
 from evolution import Evolution
@@ -7,13 +6,16 @@ from genome import Genome
 from neural_network import NeuralNetwork
 from performanceViz import PerformanceViz
 from sensor_data import SensorData
+from visualization import plot_fitness, plot_decisions
+from neural_network import create_neural_network_model
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
     try:
-        # Initialize components
+        # Neural Network and Evolutionary Components Initialization
+        model = create_neural_network_model(seq_length=10, d_model=32, action_space_size=5)  # Adjust parameters as needed
         simulator = Simulator()
         evolution = Evolution()
         neural_network = NeuralNetwork()
@@ -28,19 +30,20 @@ def main():
 
             for genome_id, genome in genomes.items():
                 logging.info(f"Simulating Genome {genome_id}")
-                
+
                 # Initialize the neural network with the current genome
                 neural_network.set_weights(genome.weights)
 
                 # Run the simulation and handle any simulation errors
                 try:
                     sensor_data = simulator.run(neural_network)
+                    processed_data = model.predict(sensor_data)  # Incorporate neural network predictions
                 except Exception as e:
                     logging.error(f"Error during simulation: {e}")
                     continue
 
                 # Evaluate the performance
-                fitness = evolution.evaluate_fitness(sensor_data)
+                fitness = evolution.evaluate_fitness(processed_data)  # Use processed data for fitness evaluation
                 evolution.update_genome_fitness(genome_id, fitness)
 
                 # Optional: Visualize performance
@@ -50,6 +53,11 @@ def main():
             evolution.evolve_generation()
 
         logging.info("Evolution process complete.")
+
+        # Visualization after completing all generations
+        fitness_over_time, decisions_over_time = performance_viz.get_data()
+        plot_fitness(fitness_over_time)
+        plot_decisions(decisions_over_time)
 
         # Optionally, save the final state or best performing genomes
         if evolution.save_final_state:
