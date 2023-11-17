de4356aa9108c13478d4b657919701d434dda456
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-11-17 00:58:04 -0500

    updated

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import logging
 from simulator import Simulator
 from evolution import Evolution
 from genome import Genome
-from neural_network import NeuralNetwork, create_neural_network_model
+from neural_network import create_neural_network_model
 from performanceViz import PerformanceViz
-from sensor_data import SensorDataHandler  # Updated import
+from sensor_data import SensorDataHandler
 from visualization import plot_fitness, plot_decisions
-import rospy  # ROS Python library
+import rospy
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -17,40 +17,40 @@ def main():
 
     try:
         # Neural Network and Evolutionary Components Initialization
-        model = create_neural_network_model(seq_length=10, d_model=32, action_space_size=5)
-        simulator = Simulator()
-        evolution = Evolution()
-        neural_network = NeuralNetwork()
-        performance_viz = PerformanceViz()
+        seq_length = 10
+        d_model = 32
+        action_space_size = 5
+        model = create_neural_network_model(seq_length, d_model, action_space_size)
         sensor_handler = SensorDataHandler()  # Initialize sensor data handler
+
+        population_size = 100
+        simulator = Simulator(population_size, seq_length, d_model)
+        evolution = Evolution(population_size, seq_length, d_model)
+        performance_viz = PerformanceViz()
 
         # Start the evolution process
         for generation in range(evolution.num_generations):
             logging.info(f"Starting Generation {generation + 1}")
 
-            genomes = evolution.create_generation()
-            for genome_id, genome in genomes.items():
-                logging.info(f"Simulating Genome {genome_id}")
-                neural_network.set_weights(genome.weights)
+            simulator.run_simulation(1)  # Run simulation for each generation
 
-                try:
-                    sensor_data = sensor_handler.get_sensor_data()  # Retrieve real sensor data
-                    processed_data = model.predict(sensor_data)  # Incorporate neural network predictions
-                except Exception as e:
-                    logging.error(f"Error during simulation: {e}")
-                    continue
+            # Evaluate and update fitness based on simulation results
+            for individual in simulator.population:
+                sensor_data = sensor_handler.get_sensor_data()  # Retrieve real sensor data
+                processed_data = model.predict(sensor_data)  # Incorporate neural network predictions
+                individual.evaluate_fitness_from_processed_data(processed_data)
+                performance_viz.update(generation, individual.genome, individual.fitness)
 
-                fitness = evolution.evaluate_fitness(processed_data)
-                evolution.update_genome_fitness(genome_id, fitness)
-                performance_viz.update(generation, genome_id, fitness)
-
-            evolution.evolve_generation()
+            evolution.evolve_population(simulator.population)
 
         logging.info("Evolution process complete.")
+
+        # Visualization after completing all generations
         fitness_over_time, decisions_over_time = performance_viz.get_data()
         plot_fitness(fitness_over_time)
         plot_decisions(decisions_over_time)
 
+        # Optionally, save the final state or best performing genomes
         if evolution.save_final_state:
             evolution.save_state("final_state.pkl")
 
