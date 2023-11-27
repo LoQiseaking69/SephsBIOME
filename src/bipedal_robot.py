b11047b4df0d792e8e6f068c57a9bc80dc3ce07d
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-11-26 20:13:31 -0500

    Update bipedal_robot.py

--- src/bipedal_robot.py
+++ src/bipedal_robot.py
@@ -1,68 +1,65 @@
-from std_msgs.msg import String, Float64
+#!/usr/bin/env python
 import rospy
+from std_msgs.msg import String, Float64
+from robot_msgs.msg import RobotStatus
 
-class BipedalRobot:
+
+class BipedalRobot():
+
     def __init__(self):
-        try:
-            # Initialize the ROS node
-            rospy.init_node('bipedal_robot_node', anonymous=True)
-        except rospy.ROSInitException as e:
-            rospy.logerr(f"ROS Init Exception: {e}")
+        rospy.init_node('bipedal_robot')  
+        self.computation_power = 100  
+        self.mobility = 100  
+        self.learning_ability = 100
+        self.energy_level = 100
+        self.max_capacity = 200
 
-        # Robot attributes
-        self.computation_power = 100  # Initial computation power
-        self.mobility = 100  # Initial mobility
-        self.learning_ability = 100  # Initial learning ability
-        self.energy_level = 100  # Energy level, affecting performance
-        self.max_capacity = 200  # Maximum capacity for attributes
-
-        # ROS publishers and subscribers
-        try:
-            self.status_publisher = rospy.Publisher('/robot_status', String, queue_size=10)
-            self.energy_subscriber = rospy.Subscriber('/energy_update', Float64, self.energy_update_callback)
-        except rospy.ROSException as e:
-            rospy.logerr(f"ROS Communication Exception: {e}")
-
-    def energy_update_callback(self, data):
-        # Update the energy level based on data received from a ROS topic
-        try:
-            new_energy_level = float(data.data)
-            if 0 <= new_energy_level <= self.max_capacity:
-                self.energy_level = new_energy_level
-                rospy.loginfo(f"Updated energy level: {self.energy_level}")
-            else:
-                rospy.logwarn("Received invalid energy level update")
-        except (ValueError, TypeError) as e:
-            rospy.logerr(f"Invalid data type in energy update: {e}")
-
-    def receive_assistance(self, individual):
-        if self.energy_level > 0:
-            self.apply_assistance(individual)
-            self.publish_status()
+        self._pub_status = rospy.Publisher('/robot/status', 
+                                            RobotStatus, queue_size=10)
+        
+        self._sub_energy = rospy.Subscriber('/robot/energy',  
+                                            Float64, self.update_energy)
+    
+        
+    def update_energy(self, data):
+        if data.data <= self.max_capacity:
+            self.energy_level = data.data
         else:
-            rospy.loginfo("Energy too low for assistance")
+            rospy.logwarn_throttle(10, "Invalid energy level input")
 
-    def apply_assistance(self, individual):
-        # Apply assistance based on individual's capabilities and robot's current state
-        self.computation_power = min(self.computation_power + self.calculate_boost(individual.assist_computation(), self.computation_power), self.max_capacity)
-        self.mobility = min(self.mobility + self.calculate_boost(individual.assist_mobility(), self.mobility), self.max_capacity)
-        self.learning_ability = min(self.learning_ability + self.calculate_boost(individual.assist_learning(), self.learning_ability), self.max_capacity)
+        self.publish_status()
 
-    def calculate_boost(self, assistance_value, current_value):
-        # Calculate the boost value based on current capability and assistance
-        boost = assistance_value * (1 - current_value / self.max_capacity)
-        return boost
+        
+    def receive_assistance(self, assistance_request):
+        if self.energy_level == 0:
+            rospy.logwarn("Cannot receive assistance without energy!")
+            return
+            
+        for skill in assistance_request:
+            self.apply_assistance(skill, assistance_request[skill])                
+        self.publish_status()
 
+        
+    def apply_assistance(self, skill, assist_value):
+        curr_capacity = getattr(self, skill + '_ability')  
+        boost = self.get_boost_value(assist_value, curr_capacity)
+        setattr(self, skill + '_ability', curr_capacity + boost) 
+ 
+       
+    def get_boost_value(self, assist_val, curr):
+        return assist_val * (1 - curr/self.max_capacity)  
+
+       
     def publish_status(self):
-        # Publish the robot's status to a ROS topic
-        status = f"Computation: {self.computation_power}, Mobility: {self.mobility}, Learning: {self.learning_ability}, Energy: {self.energy_level}"
-        self.status_publisher.publish(status)
+        status = RobotStatus()
+        status.computation = self.computation_power 
+        status.mobility = self.mobility
+        status.learning = self.learning_ability
+        status.energy = self.energy_level
+        
+        self._pub_status.publish(status)
+        
 
-    def __repr__(self):
-        return (f"Bipedal Robot - Computation: {self.computation_power}, "
-                f"Mobility: {self.mobility}, Learning: {self.learning_ability}, "
-                f"Energy: {self.energy_level}")
-
-# Example usage (uncomment when running in a ROS environment)
-# robot = BipedalRobot()
-# rospy.spin()  # Keep the node running
+if __name__ == "__main__":
+    robot = BipedalRobot()
+    rospy.spin()
