8839ef17bc98de1f4a3bac7a56c60a418bc8c394
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2023-11-28 11:28:03 -0500

    Update bipedal_robot.py
    pep8

--- src/bipedal_robot.py
+++ src/bipedal_robot.py
@@ -2,25 +2,19 @@
 import rospy
 from std_msgs.msg import String, Float64
 from robot_msgs.msg import RobotStatus
-
-
 class BipedalRobot():
 
     def __init__(self):
-        rospy.init_node('bipedal_robot')  
-        self.computation_power = 100  
-        self.mobility = 100  
+        rospy.init_node('bipedal_robot')
+        self.computation_power = 100
+        self.mobility = 100
         self.learning_ability = 100
         self.energy_level = 100
         self.max_capacity = 200
+        self._pub_status = rospy.Publisher('/robot/status', RobotStatus, queue_size=10)
 
-        self._pub_status = rospy.Publisher('/robot/status', 
-                                            RobotStatus, queue_size=10)
-        
-        self._sub_energy = rospy.Subscriber('/robot/energy',  
-                                            Float64, self.update_energy)
-    
-        
+        self._sub_energy = rospy.Subscriber('/robot/energy', Float64, self.update_energy)
+
     def update_energy(self, data):
         if data.data <= self.max_capacity:
             self.energy_level = data.data
@@ -29,36 +23,30 @@ class BipedalRobot():
 
         self.publish_status()
 
-        
     def receive_assistance(self, assistance_request):
         if self.energy_level == 0:
             rospy.logwarn("Cannot receive assistance without energy!")
             return
-            
         for skill in assistance_request:
-            self.apply_assistance(skill, assistance_request[skill])                
+            self.apply_assistance(skill, assistance_request[skill])
         self.publish_status()
 
-        
     def apply_assistance(self, skill, assist_value):
-        curr_capacity = getattr(self, skill + '_ability')  
+        curr_capacity = getattr(self, skill + '_ability')
         boost = self.get_boost_value(assist_value, curr_capacity)
-        setattr(self, skill + '_ability', curr_capacity + boost) 
- 
-       
-    def get_boost_value(self, assist_val, curr):
-        return assist_val * (1 - curr/self.max_capacity)  
+        setattr(self, skill + '_ability', curr_capacity + boost)
 
-       
+    def get_boost_value(self, assist_val, curr):
+        return assist_val * (1 - curr / self.max_capacity)
+
     def publish_status(self):
         status = RobotStatus()
-        status.computation = self.computation_power 
+        status.computation = self.computation_power
         status.mobility = self.mobility
         status.learning = self.learning_ability
         status.energy = self.energy_level
-        
         self._pub_status.publish(status)
-        
+
 
 if __name__ == "__main__":
     robot = BipedalRobot()
