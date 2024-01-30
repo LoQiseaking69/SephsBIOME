aed8bf180a667a3443bb377c330195b374f94db8
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2024-01-29 21:05:57 -0500

    refinements

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,40 +1,41 @@
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, optimizers
-import tensorflow as tf
 
+# Custom Layer for Boolformer, with added threshold parameter
 class BoolformerLayer(layers.Layer):
-    def __init__(self, **kwargs):
+    def __init__(self, threshold=0.5, **kwargs):
         super(BoolformerLayer, self).__init__(**kwargs)
+        self.threshold = threshold
 
     def build(self, input_shape):
         self.dense_layer = layers.Dense(input_shape[-1], activation='relu')
 
     def call(self, inputs):
-        logic_and = tf.math.logical_and(inputs, inputs)
+        logic_and = tf.math.logical_and(inputs, inputs > self.threshold)
         logic_transformed = self.dense_layer(logic_and)
         return logic_transformed
 
+# Updated positional encoding function with improved efficiency
 def positional_encoding(seq_length, d_model):
-    position = tf.range(seq_length)[:, tf.newaxis]
-    div_term = tf.exp(tf.range(0, d_model, 2) * -(tf.math.log(10000.0) / d_model))
+    position = tf.range(seq_length, dtype=tf.float32)[:, tf.newaxis]
+    div_term = tf.exp(tf.range(0, d_model, 2, dtype=tf.float32) * -(tf.math.log(10000.0) / d_model))
     pos_encoding = position * div_term
-    pos_encoding[:, 0::2] = tf.sin(pos_encoding[:, 0::2])
-    pos_encoding[:, 1::2] = tf.cos(pos_encoding[:, 1::2])
-    pos_encoding = pos_encoding[tf.newaxis, ...]
-    return tf.cast(pos_encoding, dtype=tf.float32)
+    pos_encoding = tf.concat([tf.sin(pos_encoding[:, 0::2]), tf.cos(pos_encoding[:, 1::2])], axis=-1)
+    return pos_encoding[tf.newaxis, ...]
 
-def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
-    x = layers.LayerNormalization(epsilon=1e-6)(inputs)
-    x = layers.MultiHeadAttention(key_dim=head_size, num_heads=num_heads, dropout=dropout)(x, x)
-    x = layers.Dropout(dropout)(x)
-    res = x + inputs
+# Enhanced transformer encoder with parameter flexibility
+def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout_rate=0.1):
+    attention_output = layers.MultiHeadAttention(key_dim=head_size, num_heads=num_heads, dropout=dropout_rate)(inputs, inputs)
+    attention_output = layers.Dropout(dropout_rate)(attention_output)
+    attention_output = layers.LayerNormalization(epsilon=1e-6)(inputs + attention_output)
 
-    x = layers.LayerNormalization(epsilon=1e-6)(res)
-    x = layers.Dense(ff_dim, activation="relu")(x)
-    x = layers.Dropout(dropout)(x)
-    x = layers.Dense(inputs.shape[-1])(x)
-    return x + res
+    ffn_output = layers.Dense(ff_dim, activation="relu")(attention_output)
+    ffn_output = layers.Dense(inputs.shape[-1])(ffn_output)
+    ffn_output = layers.Dropout(dropout_rate)(ffn_output)
+    return layers.LayerNormalization(epsilon=1e-6)(attention_output + ffn_output)
 
+# Improved QLearningLayer with additional functionality
 class QLearningLayer(layers.Layer):
     def __init__(self, action_space_size, learning_rate=0.01, gamma=0.95, **kwargs):
         super(QLearningLayer, self).__init__(**kwargs)
@@ -51,6 +52,7 @@ class QLearningLayer(layers.Layer):
             self.q_table[state, action].assign((1 - self.learning_rate) * self.q_table[state, action] + self.learning_rate * q_update)
         return tf.argmax(self.q_table[state], axis=1)
 
+# Function to create and compile the neural network model
 def create_neural_network_model(seq_length, d_model, action_space_size):
     input_layer = keras.Input(shape=(seq_length, d_model))
 
@@ -69,3 +71,10 @@ def create_neural_network_model(seq_length, d_model, action_space_size):
                   metrics={'Output': 'accuracy'})
 
     return model
+
+# Example of creating and compiling the model
+seq_length = 128  # Example sequence length
+d_model = 512     # Example dimension
+action_space_size = 10  # Example action space size
+
+model = create_neural_network_model(seq_length, d_model, action_space_size)
