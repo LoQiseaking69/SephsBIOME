1287f17fbb8063deceaf25eae37bb895390b94b6
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2024-02-05 05:24:29 -0500

    model alignment
    V5 alignment

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,8 +1,14 @@
 import tensorflow as tf
+from tensorflow.keras.utils import plot_model
 from tensorflow import keras
 from tensorflow.keras import layers, optimizers
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from tensorflow.keras.layers import Dense, Embedding, MultiHeadAttention, LayerNormalization, Dropout, LSTM, Conv1D, Input, Bidirectional, Flatten
+from tensorflow.keras.initializers import Constant
 
-# Custom Layer for Boolformer, with added trainable threshold and attention mechanism
+# Updated BoolformerLayer with LayerNormalization
 class BoolformerLayer(layers.Layer):
     def __init__(self, embedding_dim=8, num_heads=2, threshold_init_value=0.5, **kwargs):
         super().__init__(**kwargs)
@@ -14,44 +20,66 @@ class BoolformerLayer(layers.Layer):
         self.threshold = self.add_weight(
             name='threshold',
             shape=(input_shape[-1],),
-            initializer=tf.constant_initializer(self.threshold_init_value),
+            initializer=Constant(self.threshold_init_value),
             trainable=True
         )
-        self.embedding_layer = layers.Embedding(input_dim=2, output_dim=self.embedding_dim)
-        self.attention_layer = layers.MultiHeadAttention(num_heads=self.num_heads, key_dim=self.embedding_dim)
-        self.dense_layer = layers.Dense(input_shape[-1], activation='relu')
+        self.embedding_layer = Embedding(input_dim=2, output_dim=self.embedding_dim)
+        self.attention_layer = MultiHeadAttention(num_heads=self.num_heads, key_dim=self.embedding_dim)
+        self.attention_norm_layer = LayerNormalization(epsilon=1e-6)
+        self.dense_layer = Dense(input_shape[-1], activation='relu')
 
     def call(self, inputs):
         boolean_inputs = tf.greater(inputs, self.threshold)
         embeddings = self.embedding_layer(tf.cast(boolean_inputs, dtype=tf.int32))
         attention_output = self.attention_layer(embeddings, embeddings)
-        attention_output_flat = tf.reshape(attention_output, [-1, attention_output.shape[1] * self.embedding_dim])
+        attention_output_norm = self.attention_norm_layer(attention_output)
+        attention_output_flat = tf.reshape(attention_output_norm, shape=[-1, attention_output.shape[1] * self.embedding_dim])
         return self.dense_layer(attention_output_flat)
 
-# Improved QLearningLayer with additional functionality
+# Revised QLearningLayer with epsilon-greedy strategy and updated network
 class QLearningLayer(layers.Layer):
-    def __init__(self, action_space_size, learning_rate=0.01, gamma=0.95, **kwargs):
+    def __init__(self, action_space_size, state_size, learning_rate=0.01, gamma=0.95, epsilon=0.1, **kwargs):
         super().__init__(**kwargs)
         self.action_space_size = action_space_size
+        self.state_size = state_size
         self.learning_rate = learning_rate
         self.gamma = gamma
+        self.epsilon = epsilon
 
     def build(self, input_shape):
-        self.q_table = tf.Variable(initial_value=tf.random.uniform([input_shape[-1], self.action_space_size], 0, 1), trainable=True)
+        self.q_network = Dense(self.action_space_size)
 
     def call(self, state, action=None, reward=None, next_state=None):
+        q_values = self.q_network(state)
+
         if action is not None and reward is not None and next_state is not None:
-            q_update = reward + self.gamma * tf.reduce_max(self.q_table[next_state])
-            self.q_table[state, action].assign((1 - self.learning_rate) * self.q_table[state, action] + self.learning_rate * q_update)
-        return tf.argmax(self.q_table[state], axis=1)
+            next_state_q_values = self.q_network(next_state)
+            target_q_value = reward + self.gamma * tf.reduce_max(next_state_q_values, axis=1)
+            mask = tf.one_hot(action, self.action_space_size)
+
+            with tf.GradientTape() as tape:
+                current_q_values = self.q_network(state)
+                q_action = tf.reduce_sum(tf.multiply(current_q_values, mask), axis=1)
+                loss = tf.reduce_mean(tf.square(target_q_value - q_action))
+
+            grads = tape.gradient(loss, self.q_network.trainable_variables)
+            self.q_network.optimizer.apply_gradients(zip(grads, self.q_network.trainable_variables))
+
+        action_probabilities = tf.nn.softmax(q_values, axis=1)
+        chosen_action = tf.cond(
+            tf.random.uniform([], 0, 1) < self.epsilon,
+            lambda: tf.random.uniform([tf.shape(state)[0]], 0, self.action_space_size, dtype=tf.int64),
+            lambda: tf.argmax(action_probabilities, axis=1)
+        )
+        return chosen_action
 
 # Updated positional encoding function with improved efficiency
 def positional_encoding(seq_length, d_model):
     position = tf.range(seq_length, dtype=tf.float32)[:, tf.newaxis]
-    div_term = tf.exp(tf.range(0, d_model, 2, dtype=tf.float32) * -(tf.math.log(10000.0) / d_model))
+    div_term = tf.exp(tf.range(0, d_model // 2, dtype=tf.float32) * -(tf.math.log(10000.0) / d_model))
     pos_encoding = position * div_term
-    pos_encoding = tf.concat([tf.sin(pos_encoding[:, 0::2]), tf.cos(pos_encoding[:, 1::2])], axis=-1)
-    return pos_encoding[tf.newaxis, ...]
+    sin_cos_encoding = tf.concat([tf.sin(pos_encoding), tf.cos(pos_encoding)], axis=-1)
+    return sin_cos_encoding[tf.newaxis, ...]
 
 # Enhanced transformer encoder with parameter flexibility
 def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout_rate=0.1):
@@ -62,41 +90,42 @@ def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout_rate=0.1):
     ffn_output = layers.Dense(ff_dim, activation="relu")(attention_output)
     ffn_output = layers.Dense(inputs.shape[-1])(ffn_output)
     ffn_output = layers.Dropout(dropout_rate)(ffn_output)
-    encoder_output = layers.LayerNormalization(epsilon=1e-6)(attention_output + ffn_output)
-    return encoder_output
+    return layers.LayerNormalization(epsilon=1e-6)(attention_output + ffn_output)
 
 # Function to create and compile the neural network model
 def create_neural_network_model(seq_length, d_model, num_classes):
-    input_layer = keras.Input(shape=(seq_length, d_model))
+    input_layer = Input(shape=(seq_length, d_model))
 
-    # LSTM layer for handling sequential data in NLP tasks
-    x_lstm = layers.LSTM(128, return_sequences=True)(input_layer)
+    pos_encoding = positional_encoding(seq_length, 32)
 
-    # Convolutional layer for processing kinematic data
-    x_conv = layers.Conv1D(filters=32, kernel_size=3, activation='relu')(x_lstm)
+    x_lstm = Bidirectional(LSTM(128, return_sequences=True))(input_layer)
+    x_conv = Conv1D(filters=32, kernel_size=3, padding='same', activation='relu')(x_lstm)
 
-    # Generate positional encoding and add it to the convolutional output
-    pos_encoding = positional_encoding(seq_length, d_model)
     x_pos_encoded = x_conv + pos_encoding
 
-    # Transformer encoder with Conv1D output and positional encoding
     transformer_output = transformer_encoder(x_pos_encoded, head_size=32, num_heads=2, ff_dim=64)
 
-    # Custom layers (Boolformer and QLearningLayer)
+    state_size = transformer_output.shape[1] * transformer_output.shape[2]
     x_bool = BoolformerLayer()(transformer_output)
-    rl_layer = QLearningLayer(action_space_size=num_classes)(x_bool)
+    rl_layer = QLearningLayer(action_space_size=num_classes, state_size=state_size)(x_bool)
 
-    # Output layers
-    output_layer = layers.Dense(num_classes, activation='softmax', name='Output')(rl_layer)
-    reward_layer = layers.Dense(1, name='Reward')(rl_layer)
+    # Flatten the output from QLearningLayer before final dense layers
+    reshaped_output = Flatten()(rl_layer)
 
-    # Constructing the model
-    model = keras.Model(inputs=input_layer, outputs=[output_layer, reward_layer])
+    output_layer = Dense(num_classes, activation='softmax', name='Output')(reshaped_output)
+    reward_layer = Dense(1, name='Reward')(reshaped_output)
 
-    # Compiling the model
+    model = Model(inputs=input_layer, outputs=[output_layer, reward_layer])
+
     opt = optimizers.Adam(learning_rate=0.001)
     model.compile(optimizer=opt,
                   loss={'Output': 'categorical_crossentropy', 'Reward': 'mean_squared_error'},
                   metrics={'Output': 'accuracy'})
 
     return model
+
+seq_length = 128
+d_model = 512
+num_classes = 10
+
+model = create_neural_network_model(seq_length, d_model, num_classes)
